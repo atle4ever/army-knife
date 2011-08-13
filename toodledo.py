27d73f34a808cad1ffff7f1378ed6af8ad981ccd
Kim, Seongjun <atle4ever@gmail.com>
2011-08-13 12:59:13 +0900

    json parsing

--- toodledo.py
+++ toodledo.py
@@ -2,25 +2,24 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 import httplib, urllib
+import json
 
 class MainPage(webapp.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
 
         # Get session token
-        httpServ = httplib.HTTPConnection("api.toodledo.com", 80)
-        httpServ.connect()
+        httpConn= httplib.HTTPConnection("api.toodledo.com", 80)
+        httpConn.connect()
 
-        params = urllib.urlencode({'userid' : 'td4af1291294b0a', 'appid' : 'maildelivery', 'sig' : '7908c91935f48cc80635f168a8d7f592'})
-        httpServ.request('POST', '/2/account/token.php', params)
+        args = urllib.urlencode({'userid' : 'td4af1291294b0a', 'appid' : 'maildelivery', 'sig' : '7908c91935f48cc80635f168a8d7f592'})
+        httpConn.request('POST', '/2/account/token.php', args)
 
-        response = httpServ.getresponse()
-        if response.status == httplib.OK:
-            print "Output from CGI request "
-            lines = response.read().split('\n')
-            for line in lines:
-                print line.strip()
-
+        httpRes = httpConn.getresponse()
+        if httpRes.status == httplib.OK:
+            text = httpRes.read()
+            json_ = json.loads(text)
+            token = json_['token']
 
 application = webapp.WSGIApplication([('/toodledo', MainPage)], debug=True);
 
